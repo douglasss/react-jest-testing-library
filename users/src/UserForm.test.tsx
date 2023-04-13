1ed37be139cf691adaae615e4857ec194d5e5522
Douglas Silva <douglas.souza.93.ds@gmail.com>
2023-04-13 13:45:16 -0300

    [Section 2] First test

---
+++ users/src/UserForm.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows two inputs and a button', () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const inputs = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
+
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+describe('Jest', () => {
+  it('should work', () => {
+    expect(1).toBe(1);
+  });
+});
